45bc7e38f35b12b12fe06d756b010a851444cae4
anikakash <anik15-14210@diu.edu.bd>
2024-10-25 21:38:14 +0600

    add

---
+++ src/components/LogoBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-link :to="{ name: 'flights.home' }" class="navbar-brand">
+    <template v-if="!onlyLight">
+      <img class="light-mode-item navbar-brand-item" :class="customClass" :src="newLogo" alt="logo" />
+      <img
+        class="dark-mode-item navbar-brand-item"
+        :class="customClass"
+        :src="newLogo"
+        alt="logo"
+      />
+    </template>
+
+    <img v-if="onlyLight" class="navbar-brand-item" :src="logoLight" alt="logo" />
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import logo from '@/assets/images/logo.svg'
+import newLogo from '@/assets/images/travelnfare.png'
+import logoLight from '@/assets/images/logo-light.svg'
+
+defineProps({
+  customClass: String,
+  onlyLight: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
---
+++ src/views/flights/List/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavBar4 />
+
+  <main>
+    <Booking />
+
+    <NoticeBoard />
+
+    <!-- <FlightList /> -->
+  </main>
+
+  <Footer />
+
+  <BackToTop />
+</template>
+
+<script setup lang="ts">
+import BackToTop from '@/components/BackToTop.vue'
+import Footer from '@/views/flights/Home/components/Footer4.vue'
+import NavBar4 from '@/views/flights/Home/components/NavBar6.vue'
+import Booking from './components/Booking.vue'
+import FlightList from './components/FlightList.vue'
+import NoticeBoard from './components/NoticeBoard.vue'
+</script>
